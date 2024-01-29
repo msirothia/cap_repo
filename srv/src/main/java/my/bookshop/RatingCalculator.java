cf55056d4f3aed4d9cbd798f7dbbc755d6c18f91
Mayank Sirothia <m.sirothia@sap.com>
2024-01-29 10:33:33 +0000

    Added a sample comment

--- srv/src/main/java/my/bookshop/RatingCalculator.java
+++ srv/src/main/java/my/bookshop/RatingCalculator.java
@@ -19,6 +19,7 @@ import cds.gen.my.bookshop.Reviews;
 
 /**
  * This class takes care of calculating the average rating of a book based on its review ratings.
+ * Added test comment.
  */
 
 @Component

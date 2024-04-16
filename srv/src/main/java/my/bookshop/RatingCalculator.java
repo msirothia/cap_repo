d1751890fc1f2ac55783ba08fe327c1800a39061
Mayank Sirothia <m.sirothia@sap.com>
2024-04-16 06:14:48 +0000

    Sample commit for Colgate

--- srv/src/main/java/my/bookshop/RatingCalculator.java
+++ srv/src/main/java/my/bookshop/RatingCalculator.java
@@ -19,6 +19,7 @@ import cds.gen.my.bookshop.Reviews;
 
 /**
  * This class takes care of calculating the average rating of a book based on its review ratings.
+ * This is a demo push for colgate.
  * 
  */
 
